01febaacde1db17de905d0c3b61c6cb3df9761ff
Trairat Punyawatthananukool <trairat.p@skooldio.com>
2022-04-05 11:14:09 +0700

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const config = require('./config');
 
@@ -9,6 +10,12 @@ const userRouter = require('./routes/user');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 
 app.use('/users', userRouter);
 
