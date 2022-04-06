b895d35664b377305ba1528033f11cd616ad8af2
Trairat Punyawatthananukool <trairat.p@skooldio.com>
2022-04-07 02:28:52 +0700

    Refactor code to support azure app service

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const PORT = process.env.PORT || 4001;
 const config = require('./config');
 
 const userRouter = require('./routes/user');
@@ -23,8 +24,8 @@ const boot = async () => {
   // Connect to mongodb
   await mongoose.connect(config.mongoUri, config.mongoOptions);
   // Start express server
-  app.listen(4000, () => {
-    console.log('Server is running');
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
   });
 };
 
