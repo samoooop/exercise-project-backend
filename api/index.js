c9fa548ce2a880a6b582796d772dcd72791d2d27
Trairat Punyawatthananukool <trairat.p@skooldio.com>
2022-04-11 14:58:24 +0700

    Add connect mongo to app

--- api/index.js
+++ api/index.js
@@ -1,3 +1,24 @@
-const app = require('../src');
+const express = require('express');
+
+require('dotenv').config();
+const config = require('./config');
+const PORT = config.port;
+
+const userRouter = require('./routes/user');
+
+const app = express();
+
+app.use((req, res, next) => {
+  await mongoose.connect(config.mongoUri, config.mongoOptions);
+});
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
+
+app.use('/users', userRouter);
 
 module.exports = app;
